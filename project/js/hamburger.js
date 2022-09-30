6adba9a04c24d3fd26f4298466fc1f4f0cd8535b
Jeff.A <kinesthesiac@gmail.com>
2022-09-30 16:28:14 -0700

    mobile nav pane now shows and hides on click

--- project/js/hamburger.js
+++ project/js/hamburger.js
@@ -1,7 +1,14 @@
 var menus = document.getElementsByClassName('hamburger-menu');
+// var navMenu = document.getElementsByClassName('headerBorder');
 
 [].forEach.call(menus, function(m) {
     m.addEventListener('click', function() {
         m.classList.toggle('open')
-    })
+    });
 })
+
+function menuOnClick() {   
+    console.log('clicked')
+    document.getElementById('navMenu').classList.toggle('show')
+}
+
